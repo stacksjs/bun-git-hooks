4a223212a3bdc97ebe982a3b0a9d088c182855b3
Chris <chrisbreuer93@gmail.com>
2025-04-11 16:24:30 -0700

    chore: adjust config

--- git-hooks.config.ts
+++ git-hooks.config.ts
@@ -3,10 +3,10 @@ import type { GitHooksConfig } from './src/types'
 const config: GitHooksConfig = {
   'pre-commit': {
     stagedLint: {
-      '*.js': 'eslint --fix',
-      '*.{ts,tsx}': ['eslint --fix', 'prettier --write'],
-      '*.css': 'stylelint --fix',
-      '*.md': 'prettier --write'
+      '*.{js,ts,json,yaml,yml,md}': 'bunx --bun eslint . --fix',
+      // '*.{ts,tsx}': ['eslint . --fix', 'prettier --write'],
+      // '*.css': 'stylelint --fix',
+      // '*.md': 'prettier --write'
     }
   },
   // Example of a regular command hook
